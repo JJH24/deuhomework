4c3ab0221da8dfddbdb24301349f07a910420771
jjh <jjh8437@gmail.com>
2020-10-21 17:08:33 +0900

    test

---
+++ num1/divi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int divi(int a, int b){
+  if (b != 0) {
+   int result = a/b;
+   return result;
+  }
+  else{
+   printf("YOU must input Not 0\n");
+   printf("Please Try again");
+  }
+}
---
+++ num1/mult.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+ int mult(int a, int b){
+ int result = a*b;
+ return result;
+}
